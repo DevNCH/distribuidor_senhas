0d6c8021e9ce85298e77ea41331aff9489502898
VINI1206 <144918360+VINI1206@users.noreply.github.com>
2025-06-01 19:37:25 -0300

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -13,6 +13,38 @@ function formatarSenha(n) {
   return "A" + n.toString().padStart(3, '0');
 }
 
+let numero = 1;
+let letraAtual = 'A';
+let guiche = 1;
+const anteriores = [];
+
+function chamarProximaSenha() {
+  const senhaAtual = letraAtual + numero.toString().padStart(3, '0');
+  document.getElementById('senhaAtual').innerText = senhaAtual;
+  document.getElementById('guicheAtual').innerText = 'Guichê ' + guiche;
+  anteriores.unshift({ senha: senhaAtual, guiche: guiche });
+  if (anteriores.length > 3) anteriores.pop();
+  atualizarSenhasAnteriores();
+
+  numero++;
+  if (numero > 99) {
+    numero = 1;
+    letraAtual = String.fromCharCode(letraAtual.charCodeAt(0) + 1);
+  }
+  guiche = (guiche % 5) + 1;
+}
+
+function atualizarSenhasAnteriores() {
+  const container = document.getElementById('anteriores');
+  container.innerHTML = '';
+  anteriores.forEach(item => {
+    const div = document.createElement('div');
+    div.classList.add('senha-anterior');
+    div.innerHTML = `<h3>SENHA ${item.senha}</h3><p>Guichê ${item.guiche}</p>`;
+    container.appendChild(div);
+  });
+}
+
 app.post('/chamar', (req, res) => {
   const guiche = req.body.guiche || "Desconhecido";
   senhaAtual++;
