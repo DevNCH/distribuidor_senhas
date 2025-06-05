65437acb4242cd1d8ab7001e239db1d65262c316
VINI1206 <144918360+VINI1206@users.noreply.github.com>
2025-06-05 18:46:24 -0300

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -1,48 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 let senhaAtual = 0;
 let historico = [];
 
 function formatarSenha(n) {
   return "A" + n.toString().padStart(3, '0');
-}
-
-let numero = 1;
-let letraAtual = 'A';
-let guiche = 1;
-const anteriores = [];
-
-function chamarProximaSenha() {
-  const senhaAtual = letraAtual + numero.toString().padStart(3, '0');
-  document.getElementById('senhaAtual').innerText = senhaAtual;
-  document.getElementById('guicheAtual').innerText = 'Guichê ' + guiche;
-  anteriores.unshift({ senha: senhaAtual, guiche: guiche });
-  if (anteriores.length > 3) anteriores.pop();
-  atualizarSenhasAnteriores();
-
-  numero++;
-  if (numero > 99) {
-    numero = 1;
-    letraAtual = String.fromCharCode(letraAtual.charCodeAt(0) + 1);
-  }
-  guiche = (guiche % 5) + 1;
-}
-
-function atualizarSenhasAnteriores() {
-  const container = document.getElementById('anteriores');
-  container.innerHTML = '';
-  anteriores.forEach(item => {
-    const div = document.createElement('div');
-    div.classList.add('senha-anterior');
-    div.innerHTML = `<h3>SENHA ${item.senha}</h3><p>Guichê ${item.guiche}</p>`;
-    container.appendChild(div);
-  });
 }
 
 app.post('/chamar', (req, res) => {
